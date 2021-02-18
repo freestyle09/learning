cd4183b3195b0c6b2d081e0c811a430790667d89
freestyle09 <jozefrzadkosz09@gmail.com>
2021-02-18 19:35:08 +0100

    added nuxt example and routing, removed .idea

--- vue/todo/src/App.vue
+++ vue/todo/src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <Header />
-  <AddTodo v-on:add-todo="addTodo" />
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  <div>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
 import Todos from "./components/Todos.vue";
 import Header from "@/components/layout/Header";
 import AddTodo from "@/components/AddTodo";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -18,23 +21,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo one",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo two",
-          completed: false,
-        },
-        {
-          id: 3,
-          title: "Todo three",
-          completed: true,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
@@ -44,6 +31,12 @@ export default {
     deleteTodo(id) {
       this.todos = this.todos.filter((todo) => todo.id !== id);
     },
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((resp) => (this.todos = resp.data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
